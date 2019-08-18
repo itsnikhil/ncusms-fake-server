a381e1da81687a6ff2f5a03ebaa538857d4a5c76
itsnikhil <taneja.nikhil03@gmail.com>
2019-08-18 17:11:25 +0530

    hopefully added middleware

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var data = require('./data.js');
+const authMiddleware = require('./authMiddleware.js');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
+middlewares.push(authMiddleware);
 const port = process.env.PORT || 3500;
 
 server.use(middlewares);
